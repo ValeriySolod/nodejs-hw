8f88441579dd83cdcd7bc7274322e517047f3ecf
valeriy <portmone1@gmail.com>
2026-08-15 16:05:35 +0300

    feat: add MongoDB notes CRUD API

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,18 @@
+// src/routes/notesRoutes.js
+import { Router } from 'express';
+
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote,
+} from '../controllers/notesController.js';
+
+export const notesRoutes = Router();
+
+notesRoutes.get('/notes', getAllNotes);
+notesRoutes.get('/notes/:noteId', getNoteById);
+notesRoutes.post('/notes', createNote);
+notesRoutes.delete('/notes/:noteId', deleteNote);
+notesRoutes.patch('/notes/:noteId', updateNote);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
 // src/server.js
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notesRoutes } from './routes/notesRoutes.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
-});
+// Глобальні middleware
+app.use(logger);         // 1. Логер першим — бачить усі запити
+app.use(express.json()); // 2. Парсинг JSON-тіла
+app.use(cors());         // 3. Дозвіл для запитів з інших доменів
 
-// Запуск сервера
+app.use(notesRoutes);
+
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
+
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
+
+await connectMongoDB();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+

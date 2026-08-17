da8692c3c07208fd78a46d1a57909059bfbed237
valeriy <portmone1@gmail.com>
2026-08-17 11:39:38 +0300

    Fix duplicate notes router declaration

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,4 +1,3 @@
-// src/routes/notesRoutes.js
 import { Router } from 'express';
 
 import {
@@ -8,16 +7,13 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
+
 const notesRoutes = Router();
-
-// routes...
-
-export default notesRoutes;
-
-export const notesRoutes = Router();
 
 notesRoutes.get('/notes', getAllNotes);
 notesRoutes.get('/notes/:noteId', getNoteById);
 notesRoutes.post('/notes', createNote);
-notesRoutes.delete('/notes/:noteId', deleteNote);
 notesRoutes.patch('/notes/:noteId', updateNote);
+notesRoutes.delete('/notes/:noteId', deleteNote);
+
+export default notesRoutes;
